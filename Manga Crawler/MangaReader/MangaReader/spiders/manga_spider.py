839f42340b46626037164c1697f76e7a16217021
rkmaxhero <rounaqkhan12@gmail.com>
2024-05-19 16:36:55 -0400

    save progress

--- Manga Crawler/MangaReader/MangaReader/spiders/manga_spider.py
+++ Manga Crawler/MangaReader/MangaReader/spiders/manga_spider.py
@@ -8,18 +8,35 @@ class MangaLinkSpider(scrapy.Spider):
         "https://kissmanga.org/manga/manga-ny991307"
     ]
     
+    
     def parse(self, response):
+        #general info to pass as meta
+        title = response.css("h2 strong.bigChar::text").get()
+        author = response.css("p.info span::text").getall()
+        genres = response.css("p.info span::text").getall()
+        
         chapter_urls = []
-        # for chapter in response.css("h3 a"):
-        #     urls.append(chapter.css("::attr(href)").get())
         chapter_urls = response.css("h3 a::attr(href)").getall()
+        
         for url in chapter_urls:
-            yield scrapy.Request(response.urljoin(url), callback=self.parse_chapter)
+            yield scrapy.Request(response.urljoin(url), callback=self.parse_chapter, 
+                                 meta={
+                                        'title': title,
+                                        'author': author,
+                                        'genres': genres,
+                                       })
         
     def parse_chapter(self, response):
         image_urls = response.css("div#centerDivVideo img::attr(src)").getall()
         chapter_number = response.css("select#selectEpisode option[selected]::text").get().strip()[-1]
+        
         yield {
-            "Chapter_number": chapter_number,
-            "image_urls": image_urls,
+            response.meta['title']:
+                {
+                    "Chapter": chapter_number,
+                    "Images": image_urls,
+                    #"Author": response.meta['author'], #bugged extraction
+                    #"Genres": response.meta['genres'], #bugged extraction
+
+                }, 
         }
