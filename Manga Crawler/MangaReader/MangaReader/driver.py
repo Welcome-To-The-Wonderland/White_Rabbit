3dbc41fda20630149ca94cdb07d0f39a739e57b0
AshJaffer <ajaffer@umich.edu>
2024-06-03 19:57:49 -0400

    new file

--- Manga Crawler/MangaReader/MangaReader/driver.py
+++ Manga Crawler/MangaReader/MangaReader/driver.py
@@ -5,3 +5,13 @@ subprocess.run(["scrapy", "crawl", "manga", "-o", "manga.json"], check=True)
 
 # Run the Python script
 subprocess.run(["python", "sorting.py"], check=True)
+
+
+# Run the Python script and save the output to sorting_output.txt
+with open('sorting_output.txt', 'w') as sorting_output:
+    subprocess.run(
+        ["python", "sorting.py"],
+        check=True,
+        stdout=sorting_output,
+        stderr=subprocess.STDOUT
+    )
