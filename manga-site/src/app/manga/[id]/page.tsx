19e52eb02e5cb623447d36512596be86c480deb1
rkmaxhero <rounaqkhan12@gmail.com>
2024-06-09 20:51:51 -0400

    manga info page situated

--- manga-site/src/app/manga/[id]/page.tsx
+++ manga-site/src/app/manga/[id]/page.tsx
@@ -1,14 +1,58 @@
+'use client'
 import React from "react";
-interface MangaProps {
-    id: string;
-} 
+import { usePathname } from 'next/navigation'
+import manga from "./../../../manga.json"
 
-export default function Manga({ id }: MangaProps) {
+interface MangaInfo {
+  title : string;
+  cover : string;
+  chapters : Chapter[];
+}
 
+interface MangaData {
+  [key: string]: MangaInfo;
+}
+
+interface Chapter {
+  number: number;
+  images: string[];
+  uid : string
+}
+
+export default function Manga() {
+  const manga: MangaData = require("./../../../manga.json");
+  const pathname = usePathname();
+  let splitPath = pathname.split('/');
+  let out = splitPath[splitPath.length - 1];
+
+  // Find the manga that matches the 'out' variable
+  const mangaInfo = manga[out];
+  
   return (
     <div>
       <h1>Manga info</h1>
-      <h1> {id} </h1>
+      <h1>here: {out}</h1>
+
+      <h1>Title: {mangaInfo.title}</h1>
+
+      {mangaInfo && (
+        <div>
+          <h2>Chapters</h2>
+          <ul>
+            {mangaInfo.chapters.map((chapter, index) => (
+              <li key={index}> 
+                <p>{chapter.number}</p>
+                <p>{chapter.uid}</p>
+                
+                {chapter.images.map((image, index) => (
+                  <img height="100px" width= "80px" key={index} src={image} alt={`Chapter image ${index + 1}`} />
+                ))}
+
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
--- manga-site/src/app/page.tsx
+++ manga-site/src/app/page.tsx
@@ -4,16 +4,13 @@ import './home.css'
 import Link from 'next/link'
 
 export default function Home() {
-  let x = "random string order";
-  let y = "string from home"
+  let x = "manga-oh991742";
 
-  const [id, setId] = useState('');
   return (
     <div>
       <h1>Look at me</h1>
       <Link href = {{
         pathname: `/manga/${x}`,
-        query: {id: y},
       }}> some link
       </Link>
     </div>
